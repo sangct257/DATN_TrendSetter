f6ba917f4c1eb3af7939a76c8464082f64a5915d
TuanAnh <trantuananh83869999@gmail.com>
2025-03-05 18:09:36 +0700

    FE Giam gia

--- src/main/java/com/example/datn_trendsetter/Controller/Admin/DotGiamGiaController.java
+++ src/main/java/com/example/datn_trendsetter/Controller/Admin/DotGiamGiaController.java
@@ -1,12 +1,15 @@
 package com.example.datn_trendsetter.Controller.Admin;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 @Controller
 public class DotGiamGiaController {
     @RequestMapping("admin/dot-giam-gia")
     public String DotGiamGia() {
         return "Admin/DotGiamGia/hien-thi";
+    }
+
+    @RequestMapping("admin/dot-giam-gia/dot-giam-gia-chi-tiet")
+    public String DotGiamGiaChiTiet() {
+        return "Admin/DotGiamGia/dot-giam-gia-chi-tiet";
     }
 }
